7c494cf63db029337ca7a30e02e2f8c58b9c59cf
robbiblubber <alexander.lang@orf.at>
2023-05-07 20:16:40 +0200

    1.0.0 sample.

---
+++ FHTW.Swen2.VeryFirstApp/GreetingCommand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Input;
+
+
+
+namespace FHTW.Swen2.VeryFirstApp
+{
+    /// <summary>This class implements a greeting command.</summary>
+    public class GreetingCommand: ICommand
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // private members                                                                                          //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Parent view model.</summary>
+        private GreetingViewModel _VModel;
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // private members                                                                                          //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Creates a new instanec of this class.</summary>
+        /// <param name="vmodel">View model.</param>
+        public GreetingCommand(GreetingViewModel vmodel)
+        {
+            _VModel = vmodel;
+            _VModel.PropertyChanged += _VModel_PropertyChanged;
+        }
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // event handlers                                                                                           //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        private void _VModel_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(GreetingViewModel.Name)) 
+            { 
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // [interface] ICommand                                                                                     //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Raised when the CamExecute() result has changed.</summary>
+        public event EventHandler? CanExecuteChanged;
+
+
+        /// <summary>Gets if the command can be executed.</summary>
+        /// <param name="parameter">Parameter.</param>
+        /// <returns>Returns TRUE if the command can be executed, otherwise returns FALSE.</returns>
+        public bool CanExecute(object? parameter)
+        {
+            return (!string.IsNullOrWhiteSpace(_VModel.Name));
+        }
+
+
+        /// <summary>Executes the command.</summary>
+        /// <param name="parameter">Parameter.</param>
+        public void Execute(object? parameter)
+        {
+            if(!CanExecute(parameter)) return;
+
+            _VModel.GreetingText = $"Hello {_VModel.Name}!";
+        }
+    }
+}
---
+++ FHTW.Swen2.VeryFirstApp/GreetingViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+
+
+namespace FHTW.Swen2.VeryFirstApp
+{
+    /// <summary>This class implements a greeting view model.</summary>
+    public class GreetingViewModel: INotifyPropertyChanged
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // private members                                                                                          //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Greeting text.</summary>
+        private string _GreetingText = "Hello World!";
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // constructors                                                                                             //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Creates a new instance of this class.</summary>
+        public GreetingViewModel() 
+        { 
+            GreetingExecutionCommand = new GreetingCommand(this);
+        }
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // public properties                                                                                        //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        /// <summary>Gets a command that updates the greeting.</summary>
+        public ICommand GreetingExecutionCommand
+        {
+            get; private set;
+        }
+
+
+        /// <summary>Gets the name.</summary>
+        public string Name
+        {
+            get { return GreetedEntity.Instance.Name; }
+            set
+            {
+                if(GreetedEntity.Instance.Name != value) 
+                {
+                    GreetedEntity.Instance.Name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GreetingText)));
+                }
+            }
+        }
+
+
+        /// <summary>Gets or sets the greeting text.</summary>
+        public string GreetingText
+        {
+            get
+            {
+                return _GreetingText;
+            }
+            set
+            {
+                _GreetingText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(GreetingText)));
+            }
+        }
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // [interface] INotifyPropertyChanged                                                                       //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /// <summary>Raised when a property is changed.</summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
